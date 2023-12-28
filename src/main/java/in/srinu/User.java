6ca1ec694e8b11e5ae8b8557ebeecbd685b8e719
Srinu <srinubellamkonda156@email.com>
2023-12-28 09:29:15 +0530

    first commit

---
+++ src/main/java/in/srinu/User.java
@@ -0,0 +1,18 @@
+package in.srinu;
+
+import lombok.Data;
+import lombok.Setter;
+
+
+@Setter
+@Data
+public class User 
+{
+ 
+	private Integer id;
+	private String name;
+	private String lname;
+	private Double phno;
+	
+	
+}
